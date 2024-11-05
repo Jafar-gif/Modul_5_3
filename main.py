1e47f22d150f123cdcd172624778f583bc2b12fd
Jafar-gif <jafarortukov97@gmail.com>
2024-11-05 20:25:46 +0300

    worck_5_3

--- main.py
+++ main.py
@@ -1,16 +1,76 @@
-# This is a sample Python script.
+class House:
+    def __init__(self,name,nomber_of_floors):
+        self.name = name
+        self.number_of_floors = nomber_of_floors
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def go_to(self,new_floor):
+        if 1 < new_floor < self.number_of_floors:
+            for i in range(1,new_floor + 1):
+                print(i)
+            else:
+                print(' Такого этажа не сущетствует')
+    def __len__(self):
+        return self.number_of_floors
+    def __str__(self):
+        return f'Название:{self.name}, kол-во этажей:{self.number_of_floors}'
+    def __eq__(self, other):
+        if isinstance(other,House):
+            return self.number_of_floors == other.number_of_floors
+        else:
+            return False
+    def __lt__(self, other):
+        if isinstance(other,House):
+            return self.number_of_floors < other.number_of_floors
+        else:
+            return NotImplemented
+    def __le__(self, other):
+        if isinstance(other,House):
+            return self.number_of_floors <= other.number_of_floors
+        else:
+            return NotImplemented
+    def __gt__(self, other):
+        if isinstance(other,House):
+            return self.number_of_floors > other.number_of_floors
+        else:
+            return NotImplemented
+    def __ge__(self, other):
+        if isinstance(other,House):
+            return self.number_of_floors >= other.number_of_floors
+        else:
+            return NotImplemented
+    def __ne__(self, other):
+        if isinstance(other,House):
+            return self.number_of_floors != other.number_of_floors
+        else:
+            return True
+    def __add__(self, value):
+        if isinstance(value,int):
+            return House(self.name,self.number_of_floors + value)
+        elif isinstance(value,House):
+            return House(self.name,self.number_of_floors + value.number_of_floors)
+        else:
+            return NotImplemented
+    def __radd__(self, value):
+        return self.__add__(value)
+    def __iadd__(self, value):
+        return self.__add__(value)
+h1 = House('Жк Эльбрус',10)
+h2 = House('Жк Акация',20)
 
+print(h1)
+print(h2)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print(h1 == h2)
+h1 = h1 + 10
+print(h1)
+print(h1 == h2)
+h1 += 10
+print(h1)
+h2 = 10 + h2
+print(h2)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(h1 > h2)
+print(h1 >= h2)
+print(h1 < h2)
+print(h1 <= h2)
+print(h1 != h2)
